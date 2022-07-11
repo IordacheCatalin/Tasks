7b405548da0b3ef24cd55ad3b658c99400fdce24
Iordache Catalin <iordache.catalin1986@gmail.com>
2022-07-11 22:51:06 +0300

    Final Commit

--- app.js
+++ app.js
@@ -5,17 +5,21 @@ const messageDisplay = document.getElementById("message");
 submitButton.addEventListener("click", addTask);
 tasks.addEventListener("click", handleTaskClick);
 
-displayMessage("Azi am sa nu ceva de facut !");
+displayMessage("Azi am sa nu ceva de facut ?");
 
 const newTask = document.getElementById("newTask");
+const rezult1 = document.getElementById("rezult1");
 
 function addTask() {
   if (inputIsValid(newTask.value)) {
+    list.push(newTask.value);
     tasks.innerHTML +=
       `<div class="d-flex bd-highligh">
-       <li class="lista mt-3 list-group-item list-group-item-primary p-2 flex-grow-1 bd-highlight">` +
+       <div class="lista liste mt-3 list-group-item list-group-item-primary p-2 flex-grow-1 bd-highlight">` +
       newTask.value +
-      `</li><button class="lista buttonDelete bd-highlight mt-3" onclick="Delete(this);">Delete</button></div>`;
+      `</div><button class="lista buttonDelete bd-highlight mt-3" onclick="Delete(this,'` +
+      newTask.value +
+      `');">Delete</button></div>`;
     newTask.value = "";
     displayMessage("Azi chiar am ceva de facut!");
   } else {
@@ -52,6 +56,24 @@ function deleteList() {
     tasksList[0].parentNode.removeChild(tasksList[0]);
   }
 }
-function Delete(event) {
+function Delete(event, value) {
+  var index = list.indexOf(value);
+  if (index >= 0) {
+    list.splice(index, 1);
+  }
   event.parentNode.parentNode.removeChild(event.parentNode);
+}
+const btnParticipant1 = document.getElementById("btnParticipant1");
+const btnParticipant2 = document.getElementById("btnParticipant2");
+
+let list = [];
+
+function mixTasks(num, showRezultId) {
+  document.getElementById(showRezultId).style.display = "block"; //insert text in div
+
+  const shuffled = [...list].sort(() => 0.5 - Math.random()); // random mix
+
+  let x = shuffled.slice(0, num);
+  document.getElementById(showRezultId).innerHTML = x;
+  return shuffled.slice(0, num);
 }
